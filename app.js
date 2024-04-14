9fc56363f2e226ae12f437d62fd4fa38b0051cdd
Nuttayos Suvisuttikasame <nuttayossuvisuttikasame@MacBook-Pro-khxngNuttayos.local>
2024-04-14 14:37:40 +0700

    - [learn] express router

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const adminRouter = require("./routers/admin");
+const shopRouter = require("./routers/shop");
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='/product' method='POST'><input type='text' name='title' /><button type='submit'>submit</button></form>"
-  );
-});
+app.use(adminRouter);
 
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>hello from express</h1>");
-});
+app.use(shopRouter);
 
 app.listen(3000);
