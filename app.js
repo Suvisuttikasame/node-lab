eec4fcb027a1cb1ae23c9249e818214a61543509
Nuttayos Suvisuttikasame <nuttayossuvisuttikasame@MacBook-Pro-khxngNuttayos.local>
2024-04-14 17:52:59 +0700

    - [sync] sync file

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const path = require("path");
-const express = require("express");
-const bodyParser = require("body-parser");
+const path = require('path');
 
-const adminRouter = require("./routers/admin");
-const shopRouter = require("./routers/shop");
-const { get404 } = require("./controllers/errors");
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
-app.set("view engine", "pug");
-app.set("views", "views");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/admin", adminRouter);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use(shopRouter);
-
-//handle page not found error
-app.use(get404);
+app.use(errorController.get404);
 
 app.listen(3000);
--- models/product.js
+++ models/product.js
@@ -1,33 +1,40 @@
-const path = require("path");
-const fs = require("fs");
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
-  constructor(title) {
+  constructor(title, imageUrl, description, price) {
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
-    let products = [];
-    const p = path.join(__dirname, "../", "data", "product.json");
-    fs.readFile(p, (err, content) => {
-      if (!err) {
-        products = JSON.parse(content);
-        products.push(this);
-        fs.writeFile(p, JSON.stringify(products), (err) => {
-          console.log(err);
-        });
-      }
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
     });
   }
 
   static fetchAll(cb) {
-    let products = [];
-    const p = path.join(__dirname, "../", "data", "product.json");
-    fs.readFile(p, (err, content) => {
-      if (!err) {
-        products = JSON.parse(content);
-      }
-      cb(products);
-    });
+    getProductsFromFile(cb);
   }
 };
