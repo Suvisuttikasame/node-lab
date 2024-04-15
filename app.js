1bbd73dfb706b088cf7ce161e5b843f9d4b5c1e3
Nuttayos Suvisuttikasame <nuttayossuvisuttikasame@MacBook-Pro-khxngNuttayos.local>
2024-04-15 22:23:53 +0700

    - [learn] init assosiation entity

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const Product = require("./models/product");
+const User = require("./models/user");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -22,8 +24,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+//relation table
+User.hasMany(Product);
+
 sequelize
-  .sync()
+  .sync({ force: true })
   .then((result) => {
     app.listen(3000);
   })
--- models/product.js
+++ models/product.js
@@ -1,6 +1,7 @@
 const Sequelize = require("sequelize");
 
 const sequelize = require("../util/database");
+const User = require("./user");
 
 const Product = sequelize.define("product", {
   id: {
