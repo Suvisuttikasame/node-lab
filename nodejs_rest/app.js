ee11f655814bf08f184f72eb93dbdda75c9f20d9
Nuttayos Suvisuttikasame <nuttayossuvisuttikasame@MacBook-Pro-khxngNuttayos.local>
2024-04-19 23:11:40 +0700

    - [learn] cors error handling

--- nodejs_rest/app.js
+++ nodejs_rest/app.js
@@ -6,6 +6,15 @@ const feedRouter = require("./routes/feed");
 const app = express();
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Mehtods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+});
+
 app.use("/feed", feedRouter);
 
 app.listen(8080, () => {
