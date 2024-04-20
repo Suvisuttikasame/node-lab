0798c794e7afcc52625e2062542321f234bba541
Nuttayos Suvisuttikasame <nuttayossuvisuttikasame@MacBook-Pro-khxngNuttayos.local>
2024-04-20 15:29:39 +0700

    - [sync] socket.io

--- nodejs_rest/app.js
+++ nodejs_rest/app.js
@@ -64,6 +64,10 @@ mongoose
     dbName: "feed",
   })
   .then((result) => {
-    app.listen(8080);
+    const server = app.listen(8080);
+    const io = require("./socket").init(server);
+    io.on("connection", (socket) => {
+      console.log("Client connected");
+    });
   })
   .catch((err) => console.log(err));
